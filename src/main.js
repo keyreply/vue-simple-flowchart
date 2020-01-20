4ffd38700a384e7f619a66418fd03fcd294f7afd
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-01-20 18:15:28 +0700

    styling with element ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueTouch from 'vue-touch'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
+Vue.use(ElementUI);
 Vue.use(VueTouch);
 Vue.config.productionTip = false
 
